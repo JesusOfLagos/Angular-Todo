60c450893c3a03f1132ef6761a9cb74b50446d2b
JesusOfLagos <jesuswrites20043@gmail.com>
2023-08-18 19:34:11 +0100

    completed todo app.

--- ToDoListApp/src/app/app.component.ts
+++ ToDoListApp/src/app/app.component.ts
@@ -1,3 +1,5 @@
+
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ToDoListApp';
+  newTask = '';
+  tasks: { title: string; completed: boolean }[] = [];
+
+  ngOnInit() {
+    const savedTasks = localStorage.getItem('tasks');
+    if (savedTasks) {
+      this.tasks = JSON.parse(savedTasks);
+    }
+  }
+
+  addTask() {
+    if (this.newTask.trim() !== '') {
+      this.tasks.push({ title: this.newTask, completed: false });
+      this.newTask = '';
+      this.updateLocalStorage();
+    }
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+    this.updateLocalStorage();
+  }
+
+  toggleCompleted(task: any) {
+    task.completed = !task.completed;
+    this.updateLocalStorage();
+  }
+
+  clearCompletedTasks() {
+    this.tasks = this.tasks.filter(task => !task.completed);
+    this.updateLocalStorage();
+  }
+
+  updateLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
+  get activeTasks() {
+    return this.tasks.filter(task => !task.completed);
+  }
+
+  get completedTasks() {
+    return this.tasks.filter(task => task.completed);
+  }
 }
